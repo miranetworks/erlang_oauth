894ff3f99112a1b57dff573ab3d7087230519f4e
Chris de Villiers <chrisdv@miranetworks.net>
2014-06-06 15:19:31 +0200

    Major rework with simplified API.

--- src/oauth_utils.erl
+++ src/oauth_utils.erl
@@ -3,7 +3,7 @@
 % Main interface
 -export([
          init/0, init/2,
-         malformed_request/2, malformed_request/4,
+         wellformed_request/2, wellformed_request/4,
          is_authorized/3,
          get_other_params/1,
          get_consumer_key/1
@@ -13,6 +13,10 @@
 -export([
          nonce_expire/1
         ]).
+
+-ifdef(TEST).
+-compile([export_all]).
+-endif.
 
 -include("oauth_utils.hrl").
 -include_lib("stdlib/include/ms_transform.hrl").
@@ -24,6 +28,7 @@
 -define(TIMESTAMP_PARAM, "oauth_timestamp").
 -define(NONCE_PARAM, "oauth_nonce").
 -define(VERSION_PARAM, "oauth_version").
+-define(TOKEN_PARAM, "oauth_token").
 
 -define(NONCE_RETENTION_PERIOD_SECS, 15*60).  % remember nonce values by default for 15 minutes
 -define(NONCE_EXPIRE_INTERVAL_MS, 60000).     % remove expired nonce values by default every minute
@@ -58,38 +63,40 @@ init(NonceRetentionPeriodSecs, NonceExpireIntervalMs) ->
 %%
 %% @doc Convenience funs for webmachine and mochiweb that perform parameter checking.
 %%
--spec malformed_request(atom(), any()) -> {ok, #oauth_req{}} | {error, string()}.
+-spec wellformed_request(atom(), any()) -> {ok, #oauth_req{}} | {error, string()}.
 
-malformed_request(webmachine, ReqData) ->
+wellformed_request(webmachine, ReqData) ->
     Method = wrq:method(ReqData),
     Path = wrq:path(ReqData),
     QueryParams = wrq:req_qs(ReqData),
     AuthHeader = wrq:get_req_header("Authorization", ReqData),
-    malformed_request(Method, Path, QueryParams, AuthHeader);
+    wellformed_request(Method, Path, QueryParams, AuthHeader);
 
-malformed_request(mochiweb, Req) ->
+wellformed_request(mochiweb, Req) ->
     Method = Req:get(method),
     Path = Req:get(path),
     QueryParams = Req:parse_qs(),
     AuthHeader = Req:get_header_value("Authorization"),
-    malformed_request(Method, Path, QueryParams, AuthHeader).
+    wellformed_request(Method, Path, QueryParams, AuthHeader).
 
 
 %%
 %% @doc Perform parameter checking.
 %%
--spec malformed_request(atom(), string(), params_t(), string()) -> {ok, #oauth_req{}} | {error, string()}.
+-spec wellformed_request(atom(), string(), params_t(), string()) -> {ok, #oauth_req{}} | {error, string()}.
 
-malformed_request(Method, Path, QueryParams, AuthHeader) ->
-    AuthHeaderParams = parse_auth_header(AuthHeader),
-    Params = QueryParams ++ AuthHeaderParams,
-    Req = #oauth_req{
-             method       = Method,
-             path         = Path,
-             query_params = QueryParams,
-             params       = Params
-            },
-    check_params(Req).
+wellformed_request(Method, Path, QueryParams, AuthHeader) ->
+    case parse_auth_header(AuthHeader) of
+        {ok, AuthHeaderParams} ->
+            Params = QueryParams ++ AuthHeaderParams,
+            check_params(#oauth_req{
+                            method       = Method,
+                            path         = Path,
+                            query_params = QueryParams,
+                            params       = Params
+                           });
+        Error -> Error
+    end.
 
 
 %%
@@ -135,13 +142,17 @@ get_consumer_key(#oauth_req{consumer_key=ConsumerKey}) ->
 %%
 
 parse_auth_header(String) when is_list(String) ->
-    Suffix = re:replace(String, "^oauth\\s+", "", [caseless, {return,list}]),
-    Params = oauth:header_params_decode(Suffix),
-    case lists:keytake("realm", 1, Params) of
-        {value, _, ParamsExclRealm} -> ParamsExclRealm;
-        false                       -> Params
+    try
+        {match, [{0,N}]} = re:run(String, "^oauth[[:blank:]]+.", [caseless]),  % must begin with oauth
+        String2 = re:replace(string:substr(String,N), "(\\r\\n)?[[:blank:]]+", " ", [global, {return,list}]),  % strip linear white space
+        Params = oauth:header_params_decode(String2),
+        lists:keydelete("realm", 1, Params)
+    of
+        ParamsExclRealm -> {ok, ParamsExclRealm}
+    catch
+        _:_ -> {error, "Authorization header malformed"}
     end;
-parse_auth_header(_) -> [].
+parse_auth_header(_) -> {ok, []}.
 
 
 check_params(Req = #oauth_req{params=Params}) ->
@@ -169,7 +180,7 @@ verify_nonce(#oauth_req{nonce=Nonce}) ->
 
 verify_signature(Realm, ConsumerSecret, #oauth_req{method=Method, path=Path, params=Params, consumer_key=ConsumerKey, signature=Signature}) ->
     Url = Realm ++ Path,
-    {value, _, ParamsExclSignature} = lists:keytake(?SIGNATURE_PARAM, 1, Params),
+    ParamsExclSignature = lists:keydelete(?SIGNATURE_PARAM, 1, Params),
     Consumer = {ConsumerKey, ConsumerSecret, hmac_sha1},
     case oauth:verify(Signature, atom_to_list(Method), Url, ParamsExclSignature, Consumer, "") of
         true  -> ok;
@@ -183,7 +194,8 @@ oauth_keys() ->
                        ?SIGNATURE_PARAM,
                        ?TIMESTAMP_PARAM,
                        ?NONCE_PARAM,
-                       ?VERSION_PARAM]).
+                       ?VERSION_PARAM,
+                       ?TOKEN_PARAM]).
 
 
 build_oauth_param_dict(Params) ->
--- test/oauth_utils_tests.erl
+++ test/oauth_utils_tests.erl
@@ -1,6 +1,7 @@
 -module(oauth_utils_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+-include("oauth_utils.hrl").
 
 
 main_test_() ->
@@ -8,12 +9,13 @@ main_test_() ->
      fun setup/0,
      fun teardown/1,
      [
-      fun is_authorized/1,
-      fun get_header_params/1,
+      fun parse_auth_header/1,
       fun check_params/1,
-      fun get_consumer_key/1,
-      fun verify/1,
-      fun nonce_expire/1
+      fun verify_nonce/1,
+      fun verify_signature/1,
+      fun wellformed_request/1,
+      fun is_authorized/1,
+      fun getters/1
      ]}.
 
 
@@ -26,81 +28,127 @@ teardown(_) ->
     ets:delete(oauth_nonce).
 
 
-is_authorized(_) ->
-    Result1 = oauth_utils:is_authorized('GET', "http://localhost:8000", "/foo", [{"oauth_version","1.0"}, {"oauth_nonce","aupPmkqRYvOml+MLR7JlL6Syx7dL8EcTah9Aoyh5HY0="}, {"oauth_timestamp","1401710539"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"bar","baz"}], "", fun consumer_lookup/1),
-    Result2 = oauth_utils:is_authorized('GET', "http://localhost:8000", "/foo", [{"oauth_signature","w6RbTkvBEks0XIkt/HqkEudRvQ8="}, {"oauth_version","1.0"}, {"oauth_nonce","aupPmkqRYvOml+MLR7JlL6Syx7dL8EcTah9Aoyh5HY0="}, {"oauth_timestamp","1401710539"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key2"}, {"bar","baz"}], "", fun consumer_lookup/1),
-    Result3 = oauth_utils:is_authorized('GET', "http://localhost:8000", "/foo", [{"oauth_signature","w6RbTkvBEks0XIkt/HqkEudRvQ8="}, {"oauth_version","1.0"}, {"oauth_nonce","aupPmkqRYvOml+MLR7JlL6Syx7dL8EcTah9Aoyh5HY0="}, {"oauth_timestamp","1401710539"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"bar","baz"}], "", fun consumer_lookup/1),
-    Result4 = oauth_utils:is_authorized('GET', "http://localhost:8000", "/foo", [{"bar","baz"}], "OAuth oauth_signature=\"ImJG%2B%2FSO9K9CuxSWBkrvDNL4Tio%3D\", oauth_version=\"1.0\", oauth_nonce=\"ABD0Pe6ga2jAfI0lyG8dVRoQUyJuzv9d%2Fm1FSgn8K3k%3D\", oauth_timestamp=\"1401710539\", oauth_signature_method=\"HMAC-SHA1\", oauth_consumer_key=\"key\"", "secret"),
-    Result5 = oauth_utils:is_authorized('POST', "http://localhost:8000", "/foo", [], "OAuth oauth_signature=\"VDLo1TGwr6orIvj0rKJbp4YB%2BtI%3D\", oauth_version=\"1.0\", oauth_nonce=\"dT1lilc1CeN4181AgTpySpaXdTPR484EOZyP%2BWmY4Ww%3D\", oauth_timestamp=\"1401710539\", oauth_signature_method=\"HMAC-SHA1\", oauth_consumer_key=\"key\"", "secret"),
+parse_auth_header(_) ->
+    Result1 = oauth_utils:parse_auth_header(""),
+    Result2 = oauth_utils:parse_auth_header("a=\"b\""),
+    Result3 = oauth_utils:parse_auth_header("oauth a=\"b\""),
+    Result4 = oauth_utils:parse_auth_header("OAuth    a=\"b\",c=\"d\""),
+    Result5 = oauth_utils:parse_auth_header("OAuth\ta=\"b\",\r\n c=\"d\", \te=\"f\""),
+    Result6 = oauth_utils:parse_auth_header("OAuth foobar"),
+    Result7 = oauth_utils:parse_auth_header("OAuth realm=\"kings landing\",a=\"b\",c=\"d\""),
+    Result8 = oauth_utils:parse_auth_header(undefined),
     [
-     ?_assertEqual({error, "oauth_signature must be specified"}, Result1),
-     ?_assertEqual({error, "oauth_consumer_key invalid"}, Result2),
-     ?_assertEqual(ok, Result3),
-     ?_assertEqual(ok, Result4),
-     ?_assertEqual(ok, Result5)
+     ?_assertEqual({error, "Authorization header malformed"}, Result1),
+     ?_assertEqual({error, "Authorization header malformed"}, Result2),
+     ?_assertEqual({ok, [{"a","b"}]}, Result3),
+     ?_assertEqual({ok, [{"a","b"}, {"c","d"}]}, Result4),
+     ?_assertEqual({ok, [{"a","b"}, {"c","d"}, {"e","f"}]}, Result5),
+     ?_assertEqual({error, "Authorization header malformed"}, Result6),
+     ?_assertEqual({ok, [{"a","b"}, {"c","d"}]}, Result7),
+     ?_assertEqual({ok, []}, Result8)
     ].
-
-
-get_header_params(_) ->
-    Result1 = oauth_utils:get_header_params("foo=\"bar\", fiz=\"buz\""),
-    Result2 = oauth_utils:get_header_params("oauth   foo=\"bar\", fiz=\"buz\""),
-    Result3 = oauth_utils:get_header_params("OAuth foo=\"bar\", fiz=\"buz\""),
-    Result4 = oauth_utils:get_header_params("OAuth realm=\"Kings landing\", foo=\"bar\", fiz=\"buz\""),
-    [?_assertEqual([{"foo","bar"}, {"fiz","buz"}], Result) || Result <- [Result1, Result2, Result3, Result4]].
 
 
 check_params(_) ->
-    Result1 = oauth_utils:check_params([]),
-    Result2 = oauth_utils:check_params([{"oauth_consumer_key","a"}]),
-    Result3 = oauth_utils:check_params([{"oauth_consumer_key","a"}, {"oauth_signature_method","b"}]),
-    Result4 = oauth_utils:check_params([{"oauth_consumer_key","a"}, {"oauth_signature_method","b"}, {"oauth_signature","c"}]),
-    Result5 = oauth_utils:check_params([{"oauth_consumer_key","a"}, {"oauth_signature_method","b"}, {"oauth_signature","c"}, {"oauth_timestamp","d"}]),
-    Result6 = oauth_utils:check_params([{"oauth_consumer_key","a"}, {"oauth_signature_method","b"}, {"oauth_signature","c"}, {"oauth_timestamp","d"}, {"oauth_nonce","e"}]),
-    Result7 = oauth_utils:check_params([{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","c"}, {"oauth_timestamp","d"}, {"oauth_nonce","e"}, {"oauth_version","f"}]),
-    Result8 = oauth_utils:check_params([{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","c"}, {"oauth_timestamp","d"}, {"oauth_nonce","e"}, {"oauth_version","1.0"}]),
+    Result1 = oauth_utils:check_params(#oauth_req{params=[]}),
+    Result2 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}]}),
+    Result3 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","b"}]}),
+    Result4 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}]}),
+    Result5 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}]}),
+    Result6 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}]}),
+    Result7 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}]}),
+    Result8 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}, {"oauth_version","e"}]}),
+    Result9 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}, {"oauth_version","1.0"}, {"foo","bar"}]}),
+    Result10 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}, {"oauth_version","1.0"}, {"foo","bar"}, {"oauth_consumer_key","A"}]}),
+    Result11 = oauth_utils:check_params(#oauth_req{params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}, {"oauth_version","1.0"}, {"oauth_version","2.0"}, {"foo","bar"}]}),
     [
      ?_assertEqual({error, "oauth_consumer_key must be specified"}, Result1),
      ?_assertEqual({error, "oauth_signature_method must be specified"}, Result2),
-     ?_assertEqual({error, "oauth_signature must be specified"}, Result3),
-     ?_assertEqual({error, "oauth_timestamp must be specified"}, Result4),
-     ?_assertEqual({error, "oauth_nonce must be specified"}, Result5),
-     ?_assertEqual({error, "oauth_signature_method must be HMAC-SHA1"}, Result6),
-     ?_assertEqual({error, "oauth_version must be 1.0"}, Result7),
-     ?_assertEqual(ok, Result8)
+     ?_assertEqual({error, "oauth_signature_method must be HMAC-SHA1"}, Result3),
+     ?_assertEqual({error, "oauth_signature must be specified"}, Result4),
+     ?_assertEqual({error, "oauth_timestamp must be specified"}, Result5),
+     ?_assertEqual({error, "oauth_nonce must be specified"}, Result6),
+     ?_assertMatch({ok, #oauth_req{consumer_key="a", signature_method="HMAC-SHA1", signature="b", timestamp="c", nonce="d", version="1.0"}}, Result7),
+     ?_assertEqual({error, "oauth_version must be 1.0"}, Result8),
+     ?_assertMatch({ok, #oauth_req{consumer_key="a", signature_method="HMAC-SHA1", signature="b", timestamp="c", nonce="d", version="1.0"}}, Result9),
+     ?_assertEqual({error, "oauth_consumer_key specified more than once"}, Result10),
+     ?_assertEqual({error, "oauth_version specified more than once"}, Result11)
     ].
 
 
-get_consumer_key(_) ->
-    Result1 = oauth_utils:get_consumer_key([]),
-    Result2 = oauth_utils:get_consumer_key([{"oauth_consumer_key","a"}]),
-    [
-     ?_assertEqual(false, Result1),
-     ?_assertEqual("a", Result2)
-    ].
-
-
-verify(_) ->
-     Result1 = oauth_utils:verify('GET', "http://localhost:8000", "/foo", [{"oauth_signature","GX/yL/JS4+pDfYUfkq3jwe+BLtQ="}, {"oauth_version","1.0"}, {"oauth_nonce","163yQFdkKOjlFTNAdfLg9PMuQUeJEJaremtOVu1TTho="}, {"oauth_timestamp","1401708910"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"oauth_token",""}, {"bar","baz"}], "secret"),
-    Result2 = oauth_utils:verify('GET', "http://localhost:8000", "/foo", [{"oauth_signature","w6RbTkvBEks0XIkt/HqkEudRvQ8="}, {"oauth_version","1.0"}, {"oauth_nonce","aupPmkqRYvOml+MLR7JlL6Syx7dL8EcTah9Aoyh5HY0="}, {"oauth_timestamp","1401710539"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"bar","baz"}], "secret"),
-    Result3 = oauth_utils:verify('GET', "http://localhost:8000", "/foo", [{"oauth_signature","w6RbTkvBEks0XIkt/HqkEudRvQ8="}, {"oauth_version","1.0"}, {"oauth_nonce","aupPmkqRYvOml+MLR7JlL6Syx7dL8EcTah9Aoyh5HY0="}, {"oauth_timestamp","1401710539"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"bar","baz"}], "secret"),
-    Result4 = oauth_utils:verify('GET', "http://localhost:8000", "/foo", [{"oauth_signature","bogus"}, {"oauth_version","1.0"}, {"oauth_nonce","ABD0Pe6ga2jAfI0lyG8dVRoQUyJuzv9d/m1FSgn8K3k,"}, {"oauth_timestamp","1401710539"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"bar","baz"}], "secret"),
-    [
-     ?_assertEqual(ok, Result1),
-     ?_assertEqual(ok, Result2),
-     ?_assertEqual({error, "oauth_nonce has been used"}, Result3),
-     ?_assertEqual({error, "oauth_signature invalid"}, Result4)
-    ].
-
-
-nonce_expire(_) ->
-     Result1 = oauth_utils:verify_nonce([{"oauth_nonce","a"}]),
-     Result2 = oauth_utils:verify_nonce([{"oauth_nonce","a"}]),
-     timer:sleep(2000),
-     Result3 = oauth_utils:verify_nonce([{"oauth_nonce","a"}]),
+verify_nonce(_) ->
+    Result1 = oauth_utils:verify_nonce(#oauth_req{nonce="a"}),
+    Result2 = oauth_utils:verify_nonce(#oauth_req{nonce="a"}),
+    timer:sleep(2000),
+    Result3 = oauth_utils:verify_nonce(#oauth_req{nonce="a"}),
     [
      ?_assertEqual(ok, Result1),
      ?_assertEqual({error, "oauth_nonce has been used"}, Result2),
      ?_assertEqual(ok, Result3)
+    ].
+
+
+verify_signature(_) ->
+    Result1 = oauth_utils:verify_signature("http://localhost:8000", "secret", #oauth_req{method='GET', path="/foo", params=[{"oauth_signature","afYEmKQmDMK/UeC6U7mB6rDy0J8="}, {"oauth_version","1.0"}, {"oauth_nonce","XjZbboCk8P290G0G0xN5zXTNDLtmPA/zqQMCERWvE64="}, {"oauth_timestamp","1402054710"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}], consumer_key="key", signature="afYEmKQmDMK/UeC6U7mB6rDy0J8="}),
+    Result2 = oauth_utils:verify_signature("http://localhost:8000", "secret", #oauth_req{method='GET', path="/foo", params=[{"oauth_signature","yanscSW/0Go6iwd4lA4L5nGBI24="}, {"oauth_version","1.0"}, {"oauth_nonce","EkTiU5SVv8Vjdo0tNV5XOPiBZNT1tJSzOknGaDpFs4U="}, {"oauth_timestamp","1402055108"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"oauth_token",""}, {"bar","baz"}], consumer_key="key", signature="yanscSW/0Go6iwd4lA4L5nGBI24="}),
+    Result3 = oauth_utils:verify_signature("http://localhost:8000", "secret", #oauth_req{method='POST', path="/foo", params=[{"oauth_signature","Mnse+wxJ/JA32Ni/4G+KsJDIX3M="}, {"oauth_version","1.0"}, {"oauth_nonce","NUJ/Pw8x09eFIzp+7n3SzD1tktwD+R4Vt1dtZg60TIo="}, {"oauth_timestamp","1402055216"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}], consumer_key="key", signature="Mnse+wxJ/JA32Ni/4G+KsJDIX3M="}),
+    Result4 = oauth_utils:verify_signature("http://localhost:8000", "guess", #oauth_req{method='POST', path="/foo", params=[{"oauth_signature","Mnse+wxJ/JA32Ni/4G+KsJDIX3M="}, {"oauth_version","1.0"}, {"oauth_nonce","NUJ/Pw8x09eFIzp+7n3SzD1tktwD+R4Vt1dtZg60TIo="}, {"oauth_timestamp","1402055216"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}], consumer_key="key", signature="Mnse+wxJ/JA32Ni/4G+KsJDIX3M="}),
+    [
+     ?_assertEqual(ok, Result1),
+     ?_assertEqual(ok, Result2),
+     ?_assertEqual(ok, Result3),
+     ?_assertEqual({error, "oauth_signature invalid"}, Result4)
+    ].
+
+
+wellformed_request(_) ->
+    Result1 = oauth_utils:wellformed_request(m, "p", [], ""),
+    Result2 = oauth_utils:wellformed_request(m, "p", [{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}], undefined),
+    Result3 = oauth_utils:wellformed_request(m, "p", [{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}, {"foo","bar"}], undefined),
+    Result4 = oauth_utils:wellformed_request(m, "p", [], "OAuth oauth_consumer_key=\"a\", oauth_signature_method=\"HMAC-SHA1\", oauth_signature=\"b\", oauth_timestamp=\"c\", oauth_nonce=\"d\""),
+    Result5 = oauth_utils:wellformed_request(m, "p", [{"foo","bar"}], "OAuth oauth_consumer_key=\"a\", oauth_signature_method=\"HMAC-SHA1\", oauth_signature=\"b\", oauth_timestamp=\"c\", oauth_nonce=\"d\""),
+    [
+     ?_assertEqual({error, "Authorization header malformed"}, Result1),
+     ?_assertMatch({ok, #oauth_req{
+                           query_params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}],
+                           params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}]
+                          }}, Result2),
+     ?_assertMatch({ok, #oauth_req{
+                           query_params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}, {"foo","bar"}],
+                           params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}, {"foo","bar"}]
+                          }}, Result3),
+     ?_assertMatch({ok, #oauth_req{
+                           query_params=[],
+                           params=[{"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}]
+                          }}, Result4),
+     ?_assertMatch({ok, #oauth_req{
+                           query_params=[{"foo","bar"}],
+                           params=[{"foo","bar"}, {"oauth_consumer_key","a"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_signature","b"}, {"oauth_timestamp","c"}, {"oauth_nonce","d"}]
+                          }}, Result5)
+    ].
+
+
+is_authorized(_) ->
+    Result1 = oauth_utils:is_authorized("http://localhost:8000", "secret", #oauth_req{method='GET', path="/foo", params=[{"oauth_signature","afYEmKQmDMK/UeC6U7mB6rDy0J8="}, {"oauth_version","1.0"}, {"oauth_nonce","XjZbboCk8P290G0G0xN5zXTNDLtmPA/zqQMCERWvE64="}, {"oauth_timestamp","1402054710"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}], consumer_key="key", signature="afYEmKQmDMK/UeC6U7mB6rDy0J8=", nonce="XjZbboCk8P290G0G0xN5zXTNDLtmPA/zqQMCERWvE64="}),
+    Result2 = oauth_utils:is_authorized("http://localhost:8000", "secret", #oauth_req{method='GET', path="/foo", params=[{"oauth_signature","afYEmKQmDMK/UeC6U7mB6rDy0J8="}, {"oauth_version","1.0"}, {"oauth_nonce","XjZbboCk8P290G0G0xN5zXTNDLtmPA/zqQMCERWvE64="}, {"oauth_timestamp","1402054710"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}], consumer_key="key", signature="afYEmKQmDMK/UeC6U7mB6rDy0J8=", nonce="XjZbboCk8P290G0G0xN5zXTNDLtmPA/zqQMCERWvE64="}),
+    Result3 = oauth_utils:is_authorized("http://localhost:8000", fun consumer_lookup/1, #oauth_req{method='POST', path="/foo", params=[{"oauth_signature","Mnse+wxJ/JA32Ni/4G+KsJDIX3M="}, {"oauth_version","1.0"}, {"oauth_nonce","NUJ/Pw8x09eFIzp+7n3SzD1tktwD+R4Vt1dtZg60TIo="}, {"oauth_timestamp","1402055216"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}], consumer_key="key", signature="Mnse+wxJ/JA32Ni/4G+KsJDIX3M=", nonce="NUJ/Pw8x09eFIzp+7n3SzD1tktwD+R4Vt1dtZg60TIo="}),
+    Result4 = oauth_utils:is_authorized("http://localhost:8000", fun consumer_lookup/1, #oauth_req{method='POST', path="/foo", params=[{"oauth_signature","Mnse+wxJ/JA32Ni/4G+KsJDIX3M="}, {"oauth_version","1.0"}, {"oauth_nonce","NUJ/Pw8x09eFIzp+7n3SzD1tktwD+R4Vt1dtZg60TIo="}, {"oauth_timestamp","1402055216"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","hacker"}], consumer_key="hacker", signature="Mnse+wxJ/JA32Ni/4G+KsJDIX3M=", nonce="NUJ/Pw8x09eFIzp+7n3SzD1tktwD+R4Vt1dtZg60TIo="}),
+    [
+     ?_assertEqual(ok, Result1),
+     ?_assertEqual({error, "oauth_nonce has been used"}, Result2),
+     ?_assertEqual(ok, Result3),
+     ?_assertEqual({error, "oauth_consumer_key invalid"}, Result4)
+    ].
+
+
+getters(_) ->
+    Result1 = oauth_utils:get_other_params(#oauth_req{query_params=[{"oauth_signature","afYEmKQmDMK/UeC6U7mB6rDy0J8="}, {"oauth_version","1.0"}, {"oauth_nonce","XjZbboCk8P290G0G0xN5zXTNDLtmPA/zqQMCERWvE64="}, {"oauth_timestamp","1402054710"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}]}),
+    Result2 = oauth_utils:get_other_params(#oauth_req{query_params=[{"oauth_signature","yanscSW/0Go6iwd4lA4L5nGBI24="}, {"oauth_version","1.0"}, {"oauth_nonce","EkTiU5SVv8Vjdo0tNV5XOPiBZNT1tJSzOknGaDpFs4U="}, {"oauth_timestamp","1402055108"}, {"oauth_signature_method","HMAC-SHA1"}, {"oauth_consumer_key","key"}, {"oauth_token",""}, {"bar","baz"}]}),
+    Result3 = oauth_utils:get_consumer_key(#oauth_req{consumer_key="a"}),
+    [
+     ?_assertEqual([], Result1),
+     ?_assertEqual([{"bar","baz"}], Result2),
+     ?_assertEqual("a", Result3)
     ].
 
 
